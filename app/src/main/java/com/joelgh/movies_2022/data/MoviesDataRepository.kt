83686c37c3ee8d6ddd256c28f9f0c9aac8b65ba1
Joel Garrido <joelgamar@gmail.com>
2022-11-09 19:08:01 +0100

    fixed naming issues

--- app/src/main/java/com/joelgh/movies_2022/data/MoviesDataRepository.kt
+++ app/src/main/java/com/joelgh/movies_2022/data/MoviesDataRepository.kt
@@ -1,6 +1,6 @@
 package com.joelgh.movies_2022.data
 
-import com.joelgh.movies_2022.data.local.MoviesLocalSource
+import com.joelgh.movies_2022.data.local.LocalDataSource
 import com.joelgh.movies_2022.domain.MovieModel
 import com.joelgh.movies_2022.domain.MoviesRepository
 
